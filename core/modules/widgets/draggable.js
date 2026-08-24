bd722cbf4a5e352260bbea282fb930188150eecc
Simon Huber <huber.simon@protonmail.com>
2026-08-24 14:23:36 +0200

    Enhance draggable widget with per-pointer state management
    Refactor draggable widget to manage per-pointer drag states and improve DOM node handling.

--- core/modules/widgets/draggable.js
+++ core/modules/widgets/draggable.js
@@ -13,35 +13,31 @@ var Widget = require("$:/core/modules/widgets/widget.js").widget;
 
 var DraggableWidget = function(parseTreeNode,options) {
 	this.initialise(parseTreeNode,options);
+	/*
+	Per-pointer drag state.
+
+	Each active pointer gets its own state object, keyed by pointerId.
+	This allows multiple simultaneous touch/pen drags.
+	*/
+	this.dragStates = Object.create(null);
 };
 
-/*
-Inherit from the base widget class
-*/
 DraggableWidget.prototype = new Widget();
 
-/*
-Render this widget into the DOM
-*/
 DraggableWidget.prototype.render = function(parent,nextSibling) {
 	var self = this,
 		tag,
 		domNode,
 		classes = [];
-	// Save the parent dom node
+
 	this.parentDomNode = parent;
-	// Compute our attributes
 	this.computeAttributes();
-	// Execute our logic
 	this.execute();
-	// Sanitise the specified tag
 	tag = this.draggableTag;
 	if($tw.config.htmlUnsafeElements.indexOf(tag) !== -1) {
 		tag = "div";
 	}
-	// Create our element
 	domNode = this.document.createElement(tag);
-	// Assign classes
 	if(this.draggableClasses) {
 		classes.push(this.draggableClasses);
 	}
@@ -49,35 +45,52 @@ DraggableWidget.prototype.render = function(parent,nextSibling) {
 		classes.push("tc-draggable");
 	}
 	domNode.setAttribute("class",classes.join(" "));
-	// Assign data- attributes and style. attributes
+
+	/*
+	Mark the DOM node as a TiddlyWiki draggable.
+
+	This gives the synthetic drag implementation a reliable way of
+	identifying the actual draggable DOM node.
+	*/
+	domNode.setAttribute("data-tiddlywiki-draggable","yes");
 	this.assignAttributes(domNode,{
 		sourcePrefix: "data-",
 		destPrefix: "data-"
 	});
-	// Insert the node into the DOM and render any children
 	parent.insertBefore(domNode,nextSibling);
 	this.domNodes.push(domNode);
 	this.renderChildren(domNode,null);
-	// Add event handlers
+
 	if(this.dragEnable) {
 		$tw.utils.makeDraggable({
 			domNode: domNode,
-			dragTiddlerFn: function() {return self.getAttribute("tiddler");},
-			dragFilterFn: function() {return self.getAttribute("filter");},
+			dragTiddlerFn: function() {
+				return self.getAttribute("tiddler");
+			},
+			dragFilterFn: function() {
+				return self.getAttribute("filter");
+			},
 			startActions: self.startActions,
 			endActions: self.endActions,
 			dragImageType: self.dragImageType,
-			widget: this,
-			selector: self.dragHandleSelector
+			widget: self,
+			selector: self.dragHandleSelector,
+
+			/*
+			The widget owns the per-pointer state.
+
+			makeDraggable should use dragStates[event.pointerId]
+			instead of maintaining a single global drag state.
+			*/
+			dragStates: self.dragStates
 		});
 	}
 };
 
-/*
-Compute the internal state of the widget
-*/
 DraggableWidget.prototype.execute = function() {
-	// Pick up our attributes
+	/*
+	Pick up our attributes
+	*/
 	this.draggableTag = this.getAttribute("tag","div");
 	this.draggableClasses = this.getAttribute("class");
 	this.startActions = this.getAttribute("startactions");
@@ -85,47 +98,68 @@ DraggableWidget.prototype.execute = function() {
 	this.dragImageType = this.getAttribute("dragimagetype");
 	this.dragHandleSelector = this.getAttribute("selector");
 	this.dragEnable = this.getAttribute("enable","yes") === "yes";
-	// Make the child widgets
 	this.makeChildWidgets();
 };
-
 
 DraggableWidget.prototype.updateDomNodeClasses = function() {
 	var domNodeClasses = this.domNodes[0].className.split(" "),
 		oldClasses = this.draggableClasses.split(" ");
+
 	this.draggableClasses = this.getAttribute("class");
-	//Remove classes assigned from the old value of class attribute
-	$tw.utils.each(oldClasses,function(oldClass){
+	$tw.utils.each(oldClasses,function(oldClass) {
 		var i = domNodeClasses.indexOf(oldClass);
+
 		if(i !== -1) {
 			domNodeClasses.splice(i,1);
 		}
 	});
-	//Add new classes from updated class attribute.
 	$tw.utils.pushTop(domNodeClasses,this.draggableClasses);
-	this.domNodes[0].setAttribute("class",domNodeClasses.join(" "));
+	this.domNodes[0].setAttribute(
+		"class",
+		domNodeClasses.join(" ")
+	);
 };
 
-/*
-Selectively refreshes the widget if needed. Returns true if the widget or any of its children needed re-rendering
-*/
 DraggableWidget.prototype.refresh = function(changedTiddlers) {
 	var changedAttributes = this.computeAttributes();
-	if(changedAttributes.tag || changedAttributes.selector || changedAttributes.dragimagetype || changedAttributes.enable || changedAttributes.startactions || changedAttributes.endactions) {
+
+	if(
+		changedAttributes.tag ||
+		changedAttributes.selector ||
+		changedAttributes.dragimagetype ||
+		changedAttributes.enable ||
+		changedAttributes.startactions ||
+		changedAttributes.endactions
+	) {
 		this.refreshSelf();
 		return true;
 	} else {
 		if(changedAttributes["class"]) {
 			this.updateDomNodeClasses();
 		}
+
 		this.assignAttributes(this.domNodes[0],{
 			changedAttributes: changedAttributes,
 			sourcePrefix: "data-",
 			destPrefix: "data-"
 		});
 	}
+
 	return this.refreshChildren(changedTiddlers);
 };
 
-exports.draggable = DraggableWidget;
 
+/*
+Clean up active pointer states.
+
+The actual pointer cancellation/DOM restoration is handled by
+makeDraggable, but the widget must not retain references to states
+after it has been removed.
+*/
+DraggableWidget.prototype.removeChildDomNodes = function() {
+	this.dragStates = Object.create(null);
+	Widget.prototype.removeChildDomNodes.call(this);
+};
+
+
+exports.draggable = DraggableWidget;
